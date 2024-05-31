5789ce7ae579975cd75f8e9b54b74f5c215ccac0
Onke Tshaka <onktshaka200@gmail.com>
2024-05-31 15:50:38 +0200

    updated README and add a python script

---
+++ wayback_machine_scheduler.py
@@ -0,0 +1,15 @@
+import requests
+
+def save_page_now(url):
+  save_url = "https://web.archive.org/save/"
+  response =  requests.get(save_url + url)
+
+  if response.status_code == 200:
+    print(f"Successfully saved {url} to the Wayback Machine Archive")
+  else:
+    print(f"Failed to save {url}. Status code: {response.status_code}")
+
+if __name__ == "__main__":
+  #Replace with the URL you want to archive
+  url= "https://kworb.net/spotify/artist/3SozjO3Lat463tQICI9LcE_songs.html"
+  save_page_now(url)
